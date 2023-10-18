4df7f8e310b3b7981e0b188cdf41eae121874850
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-10-18 17:18:32 +0200

    Dist will be rebuild anyway; nodemon is no longer used

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,20 +1,44 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = 3000;
-app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript API!');
+const express = require('express');
+const cors = require('cors'); // Import the cors middleware
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3001;
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(bodyParser.json());
+// Dummy user data (replace this with a database)
+const users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+];
+// Login endpoint
+app.post('/login', (req, res) => {
+    const { username, password } = req.body; // Parse the user input from the JSON request body
+    // Find the user in the dummy user data (replace with database query)
+    const user = users.find((u) => u.username === username && u.password === password);
+    if (user) {
+        // Authentication successful
+        res.status(200).json({ message: 'Login successful' });
+    }
+    else {
+        // Authentication failed
+        res.status(401).json({ message: 'Login failed' });
+    }
 });
-app.get('/api', (req, res) => {
-    res.send('This is api general');
+app.post('/signup', (req, res) => {
+    const formData = req.body;
+    // Log the form data
+    console.log('Received form data:', formData);
+    if (Object.keys(formData).length === 0) {
+        return res.status(400).json({ error: 'Form data is empty' });
+    }
+    // If form data is not empty, return a 200 OK response
+    res.status(200).json({ message: 'Form data received and processed successfully' });
 });
-app.get('/api/v1', (req, res) => {
-    res.send('This is apiv1');
+// Define a simple endpoint that returns text
+app.get('/api/text', (req, res) => {
+    res.status(200).json({ "status": 'success!' });
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
