460dc1b0c053108278b876ce2d9b029b8c25ee77
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-25 21:22:20 +0200

    Attempt at creating connection pool for postgres

---
+++ backend/src/db.ts
@@ -0,0 +1,10 @@
+﻿const { Pool } = require('pg')
+const pool = new Pool({
+    host: 'postgres',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+})
+
+export default pool;
