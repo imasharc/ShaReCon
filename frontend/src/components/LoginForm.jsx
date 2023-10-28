148d1c0ec25b7935313d95ecae08197ccd4091cc
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-28 14:16:46 +0200

    Make the LoginForm in the same style as SignUp form

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom'; // Import the Link component
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/LoginForm.css'
 
 function LoginForm() {
+  const userRef = useRef();
+  const errRef = useRef(); 
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const history = useHistory(); // Initialize useHistory
+  
+  // set the focus when the component loads
+  useEffect(() => {
+    // Check if the input element exists before trying to focus on it
+    if (userRef.current) {
+      userRef.current.focus();
+    }
+  }, [])
+
+  // clear out the error message everytime any of the state pieces from dependency array change
+  useEffect(() => {
+    setError('');
+  }, [username, password])
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -40,24 +58,40 @@ function LoginForm() {
 
   return (
     <section className='LoginForm'>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-      {error && <div className="error-message">{error}</div>}
+      <p ref={errRef} className={error ? 'errmsg' : 'offscreen'} aria-live='assertive'>{error}</p>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor='username'>
+            Username:
+        </label>
+        <input
+          type='text'
+          id='username'
+          placeholder='Username'
+          ref={userRef}
+          autoComplete='off'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
 
+        <label htmlFor='password'>
+          Password:
+        </label>
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          autoComplete='off'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button>Login</button>
+        {/* {error && <div className='error-message'>{error}</div>} */}
+      </form>
       <p>
-        Don't have an account yet? <Link to="/signup">Sign up here</Link>.
+        Don't have an account yet? <Link to='/signup'>Sign up here</Link>.
       </p>
     </section>
   );
