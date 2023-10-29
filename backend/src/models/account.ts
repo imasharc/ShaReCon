00a01f169b7b0bc39864f361e97bce20ec4c48a4
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-29 16:53:46 +0100

    Reformating the backend code to fit into MVC model
    Starting with the model of the Account entity - TO BE FINISHED

---
+++ backend/src/models/account.ts
@@ -0,0 +1,17 @@
+﻿const pool = require('../config/config');
+
+const Account = {
+    // properties
+    id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+
+    // Method to retrieve an account by username
+    getByUsername: (username: string) => {
+        return pool.query('SELECT * FROM account WHERE username = $1', [username])
+      .then((result: { rows: any[]; }) => result.rows[0]);
+    }
+};
