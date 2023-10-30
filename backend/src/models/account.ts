f187fa7af7c1f3550ddac5bd7938fe516f199a25
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 10:39:14 +0100

    refactor(account-model): refactor getByUsername method
    Refactor the getByUsername method in the Account model as follows:
    
    - Use async/await for asynchronous database queries.
    - Improve error handling for database queries.
    - Consistently return the first matching account as a JSON object or null if no match is found.

--- backend/src/models/account.ts
+++ backend/src/models/account.ts
@@ -1,4 +1,4 @@
-﻿const pool = require('../config/config');
+﻿const pool = require('../db');
 
 const Account = {
     // properties
@@ -10,8 +10,23 @@ const Account = {
     password: '',
 
     // Method to retrieve an account by username
-    getByUsername: (username: string) => {
-        return pool.query('SELECT * FROM account WHERE username = $1', [username])
-      .then((result: { rows: any[]; }) => result.rows[0]);
+    getByUsername: async (username: any) => {
+        return await pool.query('SELECT * FROM account WHERE username = $1', [username])
+            .then((data: any) => {
+                if (data.rows.length > 0) {
+                    // Return the first matching account as a JSON object
+                    return data.rows[0];
+                } else {
+                    // If no matching account is found, return null
+                    return null;
+                }
+            })
+            .catch((err: any) => {
+                // Handle any errors that occur during the database query
+                console.error('Error in getByUsername:', err);
+                throw err;
+            });
     }
 };
+
+module.exports = Account;
