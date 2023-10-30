b9eeb47f37bfa92ad3f7950e3e808f116a6a5813
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 14:50:22 +0100

    feat(accountController): add updateByUsername method
    Add the updateByUsername method in the accountController as follows:
    
    - Parse the requested changes into reqUpdates constant for readability
    - Check for duplicated username or nulls in request
    - Use async/await for asynchronous database queries.
    - Improve error handling for database queries with try catch block
    - Consistently return unambiguous messages or catch an error if one occurs

--- backend/src/controllers/accountController.ts
+++ backend/src/controllers/accountController.ts
@@ -30,4 +30,38 @@ module.exports = {
             res.status(500).json({ error: 'Internal server error' });
         }
     },
+
+    // Update an account by username
+    updateByUsername: async (req: any, res: any) => {
+        const { username } = req.params; // Get the original username from the route params
+        // const { newUsername, firstName, lastName, email, password } = req.body.account; // Get the new account data from the request body
+        const reqUpdates = {
+            newUsername: req.body.account.username,
+            firstName: req.body.account.firstname,
+            lastName: req.body.account.lastname,
+            email: req.body.account.email,
+            password: req.body.account.password,
+        }
+
+        // Check if the newUsername is already taken
+        const isUsernameTaken = await Account.getByUsername(reqUpdates.newUsername);
+        const doesUsernameExist = await Account.getByUsername(username);
+
+        if (!doesUsernameExist) {
+            return res.status(400).json({ message: 'No such username' });
+        } else if (isUsernameTaken) {
+            return res.status(400).json({ message: 'Username is already taken' });
+        } else if (!reqUpdates.newUsername || !reqUpdates.firstName || !reqUpdates.lastName || !reqUpdates.email || !reqUpdates.password) {
+            return res.status(400).json({ message: 'Request fields cannot be null' });
+        }
+
+        // Proceed with the update if the newUsername is available
+        const updatedAccount = await Account.updateByUsername(username, reqUpdates.newUsername, reqUpdates.firstName, reqUpdates.lastName, reqUpdates.email, reqUpdates.password);
+
+        if (updatedAccount) {
+            res.status(200).json({ message: 'Account updated successfully', account: updatedAccount });
+        } else {
+            res.status(404).json({ message: 'Account not found' });
+        }
+    }
 };
