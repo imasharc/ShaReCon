54f985733c60a093dd26bf495f6dee0b38a8ee18
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 10:47:24 +0100

    refactor(accountController): implement getByUsername method
    Implement the 'getByUsername' method in the accountController to retrieve account data by username as follows:
    
    - Use async/await for asynchronous calls to Account.getByUsername method
    - Improve error handling for calling the Account method
    - Export the getByUsername controller method - TO BE LATER USED IN ROUTES

---
+++ backend/src/controllers/accountController.ts
@@ -0,0 +1,21 @@
+﻿const Account = require('../models/account');
+
+module.exports = {
+    // Get Account by username
+    getByUsername: async (req: any, res: any) {
+        const { username } = req.params; // Use req.params to get the username from the route params
+
+        await Account.getByUsername(username)
+            .then((data: any) => {
+                if (data) {
+                    res.status(200).json({ account: data });
+                } else {
+                    res.status(404).json({ message: 'Account not found' });
+                }
+            })
+            .catch((err: any) => {
+                console.error(err);
+                res.status(500).json({ error: 'Internal server error' });
+            });
+    }
+};
