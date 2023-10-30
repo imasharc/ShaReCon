42dd88e3c771b245ff1e0a6422c18376ceb67c59
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 10:50:29 +0100

    refactor(accountRoutes): implement router for the first application endpoint
    Implement router that operates on /user/:username endpoint calling the appropriate AccountController.getByUsername method.
    
    This method returns the user data for the selected username in JSON format

---
+++ backend/src/routes/accountRoutes.ts
@@ -0,0 +1,7 @@
+﻿﻿const express = require("express");
+const AccountController = require('../controllers/accountController');
+const router = express.Router();
+
+router.get('/user/:username', AccountController.getByUsername);
+
+module.exports = router;
