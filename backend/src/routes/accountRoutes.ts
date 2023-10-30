805894db9af99d10148f64c088dacb58e25e1c2a
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 11:41:40 +0100

    refactor(accountRoutes): implement router for the /users endpoint
    Implement router that operates on /users endpoint calling the appropriate AccountController.getAllAccunts method.
    
    This method returns all accounts data in JSON format

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -3,5 +3,6 @@ const AccountController = require('../controllers/accountController');
 const router = express.Router();
 
 router.get('/user/:username', AccountController.getByUsername);
+router.get('/users', AccountController.getAllAccounts);
 
 module.exports = router;
