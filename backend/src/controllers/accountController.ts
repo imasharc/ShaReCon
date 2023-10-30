7302ffd48a393524fc6aef206b450455eb3f1da5
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 11:35:56 +0100

    refactor(accountController): implement getAllAccounts method
    Implement the 'getAllAccounts' method in the accountController to retrieve all accounts data as follows:
    
    - Use async/await for asynchronous calls to Account.getAllAccounts method
    - Improve error handling for calling the Account method with try catch block
    - Export the the appropriate controller methods - TO BE LATER USED IN ROUTES

--- backend/src/controllers/accountController.ts
+++ backend/src/controllers/accountController.ts
@@ -1,6 +1,18 @@
 ﻿const Account = require('../models/account');
 
 module.exports = {
+        // Get all accounts
+        getAllAccounts: async (req: any, res: any) => {
+            try {
+                const accounts = await Account.getAllAccounts();
+    
+                res.status(200).json({ accounts });
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        },
+
     // Get Account by username
     getByUsername: async (req: any, res: any) => {
         const { username } = req.params; // Use req.params to get the username from the route params
