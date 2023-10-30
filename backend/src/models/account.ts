8bed8cb58a2b6cfb85ed208ba703c0b6ff6f9c00
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 11:28:58 +0100

    refactor(account-model): refactor getByUsername method
    Refactor the getByUsername method in the Account model as follows:
    
    - Replace the nested query methods (.then, .catch) with more readable try catch block and consistent const declaration

--- backend/src/models/account.ts
+++ backend/src/models/account.ts
@@ -10,22 +10,27 @@ const Account = {
     password: '',
 
     // Method to retrieve an account by username
-    getByUsername: async (username: any) => {
-        return await pool.query('SELECT * FROM account WHERE username = $1', [username])
-            .then((data: any) => {
-                if (data.rows.length > 0) {
-                    // Return the first matching account as a JSON object
-                    return data.rows[0];
-                } else {
-                    // If no matching account is found, return null
-                    return null;
-                }
-            })
-            .catch((err: any) => {
-                // Handle any errors that occur during the database query
-                console.error('Error in getByUsername:', err);
-                throw err;
-            });
+    getByUsername: async (username: string) => {
+        try {
+            const query = {
+                text: 'SELECT * FROM account WHERE username = $1',
+                values: [username],
+            };
+
+            const data = await pool.query(query);
+
+            if (data.rows.length > 0) {
+                // Return the first matching account as a JSON object
+                return data.rows[0];
+            } else {
+                // If no matching account is found, return null
+                return null;
+            }
+        } catch (err) {
+            // Handle any errors that occur during the database query
+            console.error('Error in getByUsername:', err);
+            throw err;
+        }
     }
 };
 
