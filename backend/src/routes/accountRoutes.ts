eef4b52bccfc5b073e92751c3d93eb482c185e90
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-30 14:53:10 +0100

    feat(accountRoutes): implement router for the POST /users/:username endpoint
    Implement router that operates on /users/:username endpoint calling the appropriate AccountController.updateByUsername method.
    
    This method updates specific (based on username) account with requested changes and returns updates data in JSON format

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get('/:username', AccountController.getByUsername);
 router.get('/', AccountController.getAllAccounts);
+router.put('/:username', AccountController.updateByUsername);
 
 module.exports = router;
