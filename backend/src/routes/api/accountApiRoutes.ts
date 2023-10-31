331abb41bba1103ff67aa2568f56795e46ffa5cf
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-10-31 13:11:46 +0100

    refactor(accountApiRoutes): remove unnecessary endpoint call

--- backend/src/routes/api/accountApiRoutes.ts
+++ backend/src/routes/api/accountApiRoutes.ts
@@ -3,7 +3,6 @@ const AccountController = require('../../controllers/accountController');
 const router = express.Router();
 
 router.get('/:username', AccountController.getByUsername);
-router.post('/', AccountController.loginByUsername);
 router.get('/', AccountController.getAllAccounts);
 router.post('/', AccountController.createNew);
 router.put('/:username', AccountController.updateByUsername);
