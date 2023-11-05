379d878818e81775577a9ee37cff61e80efcc77d
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-11-05 16:13:17 +0100

    feat(App.js): add method for checking if the available cookie belongs to some account
    If yes, redirect to accountPanel INSTEAD OF the login page - DOES NOT WORK YET

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 import AccountPanel from "./components/AccountPanel";
@@ -16,11 +22,37 @@ function Home() {
 }
 
 function App() {
-  const [, , removeCookie] = useCookies(["token"]); // Replace 'token' with your cookie name
+  const history = useHistory(); // Initialize useHistory
+  const [cookie, setCookie, removeCookie] = useCookies(); // Replace 'token' with your cookie name
 
   const handleLogout = () => {
     removeCookie("token"); // Remove the 'token' cookie
   };
+
+  const handleAuth = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/users/isValid/${cookie.token}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        if (data.account.token === cookie.token) {
+          console.log(data.account.username);
+          console.log(cookie.token);
+          history.push(`/account/${data.account.username}`);
+        } else {
+          console.error("Authorization failed");
+        }
+      } else {
+        throw new Error("Failed to fetch user data");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="app-container">
       <Router>
@@ -28,7 +60,7 @@ function App() {
           <Link className="nav-link" to="/">
             Home
           </Link>
-          <Link className="nav-link" to="/login">
+          <Link className="nav-link" to="/login" onClick={handleAuth}>
             Login
           </Link>
           <Link className="nav-link" to="/" onClick={handleLogout}>
