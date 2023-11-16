3b130e9542622e1224d35e126645cdaf47935b65
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-11-16 11:32:17 +0100

    feat(PostPrompt): Log the text prompt and active cookie to the console
    Get the active cookie/token and associate it with the text prompt to be uploaded

--- frontend/src/components/PostPrompt.jsx
+++ frontend/src/components/PostPrompt.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
 import '../styles/PostPrompt.css';
 
-const PostPrompt = ({ onPromptSubmit }) => {
-  const handlePromptSubmit = (e) => {
+const PostPrompt = ({ onPromptSubmit, authToken }) => {
+  const [text, setText] = useState('');
+  // Get the 'token' cookie
+  const [cookies] = useCookies(['token']);
+
+  const handlePromptSubmit = async (e) => {
     e.preventDefault();
-    // Add logic to handle the prompt submission
-    onPromptSubmit(e.target.elements.prompt.value);
+    
+    try {
+      const authToken = cookies.token;
+      // Add logic to handle the prompt submission (including auth token)
+      await onPromptSubmit({ text, authToken });
+      // Clear the input
+      setText('');
+    } catch (error) {
+      console.error('Error submitting post:', error);
+    }
   };
 
   return (
     <div className="post-prompt-container">
       <form onSubmit={handlePromptSubmit}>
-          <input type="text" name="prompt" placeholder="What's on your mind?"/>
+          <input
+          type="text"
+          name="prompt"
+          placeholder="What's on your mind?"
+          value={text}
+          onChange={(e) => setText(e.target.value)}/>
         <button type="submit">Submit</button>
       </form>
     </div>
