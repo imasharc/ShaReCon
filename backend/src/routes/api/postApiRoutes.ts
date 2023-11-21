e7d79795479ed016fdec36de5f21565700b70c59
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-11-21 14:28:25 +0100

    feat(postApiRoutes): Add router methods for Api communication, using postController methods

---
+++ backend/src/routes/api/postApiRoutes.ts
@@ -0,0 +1,11 @@
+﻿const express = require("express");
+const PostContoller = require('../../controllers/postController');
+const router = express.Router();
+
+router.get('/:id', PostContoller.getById);
+router.get('/', PostContoller.getAllPosts);
+router.post('/', PostContoller.createNew);
+router.put('/:id', PostContoller.updateById);
+router.delete('/:id', PostContoller.deleteById);
+
+module.exports = router;
