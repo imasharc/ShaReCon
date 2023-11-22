30d07fad5555b8675609b36db2bba3b85fcaebcc
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-11-22 10:10:55 +0100

    feat(PostPrompt): Add uploading funtionality for text posts

--- frontend/src/components/PostPrompt.jsx
+++ frontend/src/components/PostPrompt.jsx
@@ -12,10 +12,40 @@ const PostPrompt = ({ onPromptSubmit, authToken }) => {
     
     try {
       const authToken = cookies.token;
-      // Add logic to handle the prompt submission (including auth token)
-      await onPromptSubmit({ text, authToken });
-      // Clear the input
-      setText('');
+      
+      // Check if there's text before making the request
+      if (!text) {
+        console.error('Please enter some text.');
+        return;
+      }
+
+      const response = await fetch('http://localhost:3001/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "post": {
+            "text_content": text,
+            "token": authToken
+        }
+    }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to submit post: ${response.status}`);
+    }
+
+    // Assuming your backend returns the newly created post
+    const newPost = await response.json();
+
+    // Add logic to handle the new post (e.g., update state, refresh posts, etc.)
+    // For now, let's log the new post
+    console.log('New Post:', newPost);
+
+    // Clear the input
+    setText('');
+
     } catch (error) {
       console.error('Error submitting post:', error);
     }
